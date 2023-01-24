2c58b6db9a9844802cdb5b04c1cdb41a341545b6
IgorD.Souza <igor251999@hotmail.com>
2023-01-24 10:01:06 -0300

    construção da logica dos botões

--- scripts/controller/calcController.js
+++ scripts/controller/calcController.js
@@ -10,6 +10,8 @@ class CalcController{
 
         this._timeEl = document.querySelector("#hora")
 
+        this._operation= [];
+
         this.initButtonsEvents();
 
         this._currentDate;
@@ -33,12 +35,63 @@ class CalcController{
             });
         }
 
+        execBtn(value){
+        
+            switch(value){
+                case 'ac':
+                    this.clearAll();
+                    break;
+    
+                case 'ce':
+                    this.clearEntry();
+                    break;
+    
+                case 'soma':
+    
+                    break;
+    
+                case 'subtracao':
+                    break;
+    
+                case 'divisao':
+                    break;
+    
+                case 'mutiplicacao':
+                    break;
+    
+                case 'porcento':
+                    break;
+    
+                case 'igual':
+                    break;
+                
+                case '0':
+                case '1':
+                case '2':
+                case '3':
+                case '4':
+                case '5':
+                case '6':
+                case '7':
+                case '8':
+                case '9':
+                    this.addOperation(parseInt(value));
+                    break;
+    
+                default:
+                    this._setError()
+                    break;
+            }
+        }
+
     initButtonsEvents(){
         let buttons = document.querySelectorAll("#buttons>g , #parts> g ");
 
         buttons.forEach((btn,index)=>{
             this.addEventListenerAll/*Criado por nós*/(btn,"click drag ", e =>{
-                console.log(btn.className.baseVal.replace("btn-",''));
+                var textBtn = btn.className.baseVal.replace("btn-",'');
+
+                this.execBtn(textBtn);
             });
 
             this.addEventListenerAll(btn,'mouseover mouseup mousedown', e=>{
@@ -52,6 +105,27 @@ class CalcController{
         this.displayDate = this.currentDate.toLocaleDateString(this._locale,{day:"2-digit", month:"long", year:"numeric"});
             this.displayTime = this.currentDate.toLocaleTimeString(this._locale);
     }
+
+    clearAll(){
+        this._operation=[];
+    }
+
+    clearEntry(){
+        this._operation.pop();
+    }
+    
+    addOperation(value){
+        this._operation.push(value);
+
+        console.log(this._operation)
+
+    }
+
+    setError(){
+        this.displayCalc = 'Error';
+    }
+
+   
 
     get displayDate(){
         return this._dateEl.innerHTML;
@@ -71,15 +145,15 @@ class CalcController{
         return this._displayCalcEl.innerHTML;
     }
     set displayCalc(value){
-        this._displayCalcEl.innerHTML = valor;
+        this._displayCalcEl.innerHTML = value;
     }
 
     get currentDate(){
          
         return new Date;
     }
-    set currentDate(valor){
-        this._currentDate = valor;
+    set currentDate(value){
+        this._currentDate = value;
     }
 
 }
